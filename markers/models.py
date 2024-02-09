a3c33d11c315870ed3b4cbe673f52e26fbfddde8
Yuriy Babin <babin.yurii@gmail.com>
2024-02-09 10:33:08 +0300

    edit markers

--- markers/models.py
+++ markers/models.py
@@ -1,17 +1,8 @@
 from django.db import models
 from django.core.exceptions import ValidationError
-from django.core.validators import URLValidator
+from django.core.validators import URLValidator, MinValueValidator, MaxValueValidator
 from django.utils.timezone import now
 from users.models import CustomUser
-
-
-def validate_rs(value):
-    if not value.startswith('rs'):
-        raise ValidationError(f'{value} should start with "rs"')
-
-def validate_freq(value):
-    if value >= 1 or value <= 0:
-        raise ValidationError('frequency value should be in the range between 0 and 1')
 
 
 class SingleNucPol(models.Model):
@@ -27,17 +18,17 @@ class SingleNucPol(models.Model):
         (THYMINE, 'T'),
     )
     
-    rs = models.CharField(max_length=20, verbose_name='rs id', unique=True, validators=[validate_rs, ])
+    rs = models.CharField(max_length=20, verbose_name='rs id', unique=True)
     gene_name_short = models.CharField(max_length=20, verbose_name='short gene name')
     gene_name_full = models.CharField(max_length=255, blank=True, null=True, verbose_name='full gene name')
     nuc_var_1 = models.CharField(max_length=1, choices=NUC_CHOICES, verbose_name='allele 1')
     nuc_var_2 = models.CharField(max_length=1, choices=NUC_CHOICES, verbose_name='allele 2')
     nuc_var_1_freq = models.FloatField(blank=True, null=True, verbose_name='allele 1 frequency (from dbSNP)',
                                         help_text='floating point number. f.e.: 0.5',
-                                        validators=[validate_freq, ])
+                                        validators=[MinValueValidator(0), MaxValueValidator(1) ])
     nuc_var_2_freq = models.FloatField(blank=True, null=True, verbose_name='allele 2 frequency (from dbSNP)',
                                         help_text='floating point number. f.e.: 0.5',
-                                        validators=[validate_freq, ])
+                                        validators=[MinValueValidator(0), MaxValueValidator(1) ])
     db_snp_link = models.URLField(max_length=255, blank=True, null=True, unique=True, verbose_name='dbSNP link',
                                     validators=[URLValidator, ])
     date_created = models.DateTimeField(auto_now_add=True, editable=False)
@@ -50,7 +41,10 @@ class SingleNucPol(models.Model):
         if all([self.nuc_var_1_freq, self.nuc_var_2_freq]): 
             if sum([self.nuc_var_1_freq, self.nuc_var_2_freq]) != 1:
                 raise ValidationError('allele frequency sum should be equal to 1')
-
+        if not self.rs.startswith('rs'):
+            raise ValidationError(f'{self.rs} should start with "rs"')
+        if not self.rs[2:].isnumeric():
+            raise ValidationError(f'rs id field should contain only numbers after "rs"')
 
     def __str__(self):
         return f'{self.rs} {self.gene_name_short} {self.nuc_var_1}/{self.nuc_var_2}'
