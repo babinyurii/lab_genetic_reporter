5da6f6e0d54a599f59a4a3ed1797beb4fdaa3804
Yuriy Babin <babin.yurii@gmail.com>
2024-07-19 14:36:11 +0300

    NUC_CHOICES, ALLELE_CLIN_SIGNIF_CHOICE into constants

---
+++ markers/constants.py
@@ -0,0 +1,23 @@
+ADENINE = 'A'
+CYTOSINE = 'C'
+GUANINE = 'G'
+THYMINE = 'T'
+MMP3_al_1 = '5A'
+MMP3_al_2 = '6A'
+MMP1_del = '_'
+
+NUC_CHOICES = (
+        (ADENINE, 'A'),
+        (CYTOSINE, 'C'),
+        (GUANINE, 'G'),
+        (THYMINE, 'T'),
+        (MMP3_al_1, '5A'),
+        (MMP3_al_2, '6A'),
+        (MMP1_del, '_'),
+    )
+
+ALLELE_CLIN_SIGNIF_CHOICE = (
+        ('NORM', 'NORM'),
+        ('RISK', 'RISK'),
+        ('UNKNOWN', 'UNKNOWN'),
+    )
