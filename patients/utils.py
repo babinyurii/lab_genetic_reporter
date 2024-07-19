e11a5fa0ace1f17ef145cd71d8004c02b8d8bdac
Yuriy Babin <babin.yurii@gmail.com>
2024-07-19 14:59:18 +0300

    placeholder for conclusion, if category was not chosen or conclusion was not created by user

--- patients/utils.py
+++ patients/utils.py
@@ -1,31 +1,40 @@
 
 
 def generate_text_for_conclusion(two_snp_conc, one_snp_conc):
-        sep = '\n'
-        space = ' '
-        text = ''
-        category_sep = '='
-        snp_sep = '-'
-        
-        for report in two_snp_conc['two_snp_reports']:
-            text += f"SNP1: {report['snp_1_rs']} {space} {report['snp_1_gene']}{space} \
-                {report['snp_1_result']}{space} {sep} SNP2:  {report['snp_2_rs']}{space} {report['snp_2_gene']}{space}\
-                     {report['snp_2_result']}"
-            text += sep * 2
-            text += report['conc']
-            text += sep * 2 
-            text += snp_sep * 50 + sep
-        text += category_sep * 50 + sep
-
-        for category in one_snp_conc.keys():
+    sep = '\n'
+    space = ' '
+    text = ''
+    category_sep = '='
+    snp_sep = '-'
+    
+    for report in two_snp_conc['two_snp_reports']:
+        text += f"SNP1: {report['snp_1_rs']} {space} {report['snp_1_gene']}{space} \
+            {report['snp_1_result']}{space} {sep} SNP2:  {report['snp_2_rs']}{space} {report['snp_2_gene']}{space}\
+                    {report['snp_2_result']}"
+        text += sep * 2
+        text += report['conc']
+        text += sep * 2 
+        text += snp_sep * 50 + sep
+    text += category_sep * 50 + sep
+    
+    print(one_snp_conc, flush=True)
+    for category in one_snp_conc.keys():
+        if category is not None:
             text += category
-            text += sep * 2
-            reports = one_snp_conc[category]
-            for report in reports:
-                text += f"{report['rs']} {space}{report['gene']} {space}{report['genotype']}"
-                text += sep * 1
+        # TODO logging: if category not chosen before - into warning
+        else:
+            text += 'None, no category selected'
+        text += sep * 2
+        reports = one_snp_conc[category]
+        for report in reports:
+            text += f"{report['rs']} {space}{report['gene']} {space}{report['genotype']}"
+            text += sep * 1
+            if report['conc'] is not None:
                 text += report['conc']
-                text += sep * 2
-                text += snp_sep * 50 + sep
-                
-        return text
+            # TODO logging: if conclusion was not created by user not chosen before - into warning
+            else:
+                text += 'None, no conclusion is db'
+            text += sep * 2
+            text += snp_sep * 50 + sep
+            
+    return text
