4871c56e342988bb9d84769c899bfd60764cab68
Yuriy Babin <babin.yurii@gmail.com>
2024-07-19 14:51:25 +0300

    edit generate_text_for_conclusion

--- patients/utils.py
+++ patients/utils.py
@@ -4,13 +4,18 @@ def generate_text_for_conclusion(two_snp_conc, one_snp_conc):
         sep = '\n'
         space = ' '
         text = ''
+        category_sep = '='
+        snp_sep = '-'
         
         for report in two_snp_conc['two_snp_reports']:
-            text += f"{report['snp_1_rs']} {space} {report['snp_1_gene']}{space} {report['snp_1_result']}{space} {report['snp_2_rs']}{space} {report['snp_2_gene']}{space} {report['snp_2_result']}"
+            text += f"SNP1: {report['snp_1_rs']} {space} {report['snp_1_gene']}{space} \
+                {report['snp_1_result']}{space} {sep} SNP2:  {report['snp_2_rs']}{space} {report['snp_2_gene']}{space}\
+                     {report['snp_2_result']}"
             text += sep * 2
             text += report['conc']
-            text += sep * 2
-        text += sep * 3
+            text += sep * 2 
+            text += snp_sep * 50 + sep
+        text += category_sep * 50 + sep
 
         for category in one_snp_conc.keys():
             text += category
@@ -18,7 +23,9 @@ def generate_text_for_conclusion(two_snp_conc, one_snp_conc):
             reports = one_snp_conc[category]
             for report in reports:
                 text += f"{report['rs']} {space}{report['gene']} {space}{report['genotype']}"
-                text += sep * 2
+                text += sep * 1
                 text += report['conc']
-       
+                text += sep * 2
+                text += snp_sep * 50 + sep
+                
         return text
